b99896bb5758287d107b3283aa46aad8a2890cea
lapayne <56700654+lapayne@users.noreply.github.com>
2022-04-02 19:40:25 +0100

    add email function

---
+++ api/sendEmail/index.js
@@ -0,0 +1,13 @@
+module.exports = function (context, input) {
+    var message = {
+        "personalizations": [ { "to": [ { "email": "lee@thepaynetrain.com" } ] } ],
+        from: { email: "contact@thepaynetrain.com" },
+        subject: "contact form",
+        content: [{
+            type: 'text/plain',
+            value: input
+        }]
+    };
+
+    return message;
+};
