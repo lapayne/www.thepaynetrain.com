1bbceff54726ecc03c3ebc47d459fb83a57e2808
Lee Payne <lapayne@gmail.com>
2022-04-09 15:49:54 +0000

    Updated index.js

--- api/sendEmail/index.js
+++ api/sendEmail/index.js
@@ -1,14 +1,5 @@
 
 module.exports = async function (context, req) {
 
-    context.bindings.message = {
-        subject: 'Contact form - thepaynetrain',
-        content: [{
-            type: 'text/plain',
-            value: "test body"
-        }]
-    };
-    context.res = {
-        body: {text: "works"},
-        };
-};
+    context.log("testing");
+}
