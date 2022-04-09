cc6b3273773b8bb8720c82da1c5c9ada8d35307c
lapayne <56700654+lapayne@users.noreply.github.com>
2022-04-09 21:24:42 +0100

    change binding

--- api/sendEmail/index.js
+++ api/sendEmail/index.js
@@ -1,6 +1,5 @@
 
 module.exports = async function (context, req) {
     var message = 'Service Bus queue message created at ';
-        context.log(message);   
         context.bindings.sbMessage = message;
     }
