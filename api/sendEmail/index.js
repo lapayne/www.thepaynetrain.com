d54f15dd67d7e6ae88aae500a97b55c01fbf1b4f
lapayne <56700654+lapayne@users.noreply.github.com>
2022-04-10 19:18:59 +0100

    copied from cloud

--- api/sendEmail/index.js
+++ api/sendEmail/index.js
@@ -1,5 +1,6 @@
 
 module.exports = async function (context, req) {
-    var message = 'Service Bus queue message created at '; 
+    var message = 'Service Bus queue message created at ';
+        context.log(message);   
         context.bindings.sbMessage = message;
     }
