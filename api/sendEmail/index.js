e3158a5b8380727d01864b0248a1133c4ef9bb25
lapayne <56700654+lapayne@users.noreply.github.com>
2022-04-17 20:53:11 +0100

    redirect try

--- api/sendEmail/index.js
+++ api/sendEmail/index.js
@@ -1,3 +1,9 @@
 module.exports = async function (context, req) {
     context.bindings.outputSbQueue = req.body;
+    context.res = {
+        status: 302,
+        headers: {
+            'Location': 'https://www.thepaynetrain.com/?name=Message sent successfully.'
+        }
+    };
 };
