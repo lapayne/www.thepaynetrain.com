68a67fcfbde08d9e42eae96f7065a293360020e1
Lee Payne <lee.payne@bjss.com>
2022-08-09 15:40:35 +0100

    Added quote getter

---
+++ api/GetQuote/index.js
@@ -0,0 +1,48 @@
+module.exports = async function (context, req) {
+    
+    fetch(window.location.origin + "/quotes")
+    .then(response => response.body)
+  .then(rb => {
+    const reader = rb.getReader();
+  
+    return new ReadableStream({
+      start(controller) {
+        // The following function handles each data chunk
+        function push() {
+          // "done" is a Boolean and value a "Uint8Array"
+          reader.read().then( ({done, value}) => {
+            // If there is no more data to read
+            if (done) {
+              console.log('done', done);
+              controller.close();
+              return;
+            }
+            // Get the data and send it to the browser via the controller
+            controller.enqueue(value);
+            // Check chunks by logging to the console
+            console.log(done, value);
+            push();
+          })
+        }
+  
+        push();
+      }
+    });
+  })
+  .then(stream => {
+    // Respond with our stream
+    return new Response(stream, { headers: { "Content-Type": "text/text" } }).text();
+  })
+  .then(result => {
+    // Do things with result
+    const quotearray = result.split(";;")
+    const responseMessage = quotearray[Math.floor(Math.random() * quotearray.length-2)];
+  });
+    
+    
+      
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: responseMessage
+    };
+}
