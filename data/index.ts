965cf71c87fd9efa8e2368ad5c0bdf74de9d1896
Lee Payne <56700654+lapayne@users.noreply.github.com>
2024-12-30 15:16:18 +0000

    Update index.ts

--- data/index.ts
+++ data/index.ts
@@ -31,14 +31,16 @@ export const socialMedia = [
 export const projects = [
   {
     id: 1,
-    img: "",
-    href: "",
-    description: "",
+    img: "https://images.unsplash.com/photo-1544077960-604201fe74bc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1651&q=80",
+    href: "https://www.maxpayne.vip",
+    description: "A homepage showing off all of the projects he has completed",
+    name: "Projects",
   },
   {
     id: 2,
-    img: "",
-    href: "",
-    description: "",
+    img: "https://images.unsplash.com/photo-1544077960-604201fe74bc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1651&q=80",
+    href: "maps.maxpayne.vip",
+    description: "A map of everywhere our family has",
+    name: "Maps",
   },
 ]
