4378ad2c4e346bbfef974af00df315c265362a38
Lee Payne <56700654+lapayne@users.noreply.github.com>
2024-12-30 15:14:08 +0000

    Update index.ts

--- data/index.ts
+++ data/index.ts
@@ -1,110 +1,13 @@
 export const navItems = [
-  { name: "About", link: "#about" },
+  { name: "Education", link: "#education" },
+  { name: "Experience", link: "#experience" },
+  { name: "Projects", link: "#projects" },
+  { name: "Contact", link: "#contact" },
   {
     name: "Resume",
-    link: "https://drive.google.com/file/d/1udileHqcrbD09gjuFpUZlHgpOajw3UU_/view?usp=sharing",
+    link: "/cv.pdf",
   },
-  { name: "Projects", link: "#projects" },
-  //{ name: "Testimonials", link: "#testimonials" },
-  { name: "Blogs", link: "https://dev.to/akashjana" },
-  { name: "Contact", link: "#contact" },
-];
-
-export const gridItems = [
-  {
-    id: 1,
-    title:
-      "I prioritize team/client collaboration, fostering open communication ",
-    description: "",
-    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
-    imgClassName: "w-full h-full",
-    titleClassName: "justify-end",
-    img: "/b1.svg",
-    spareImg: "",
-  },
-  {
-    id: 2,
-    title: "I'm very flexible with time zone communications",
-    description: "",
-    className:
-      "lg:col-span-2 md:col-span-3 md:row-span-2 min-h-[40vh] select-none",
-    imgClassName: "",
-    titleClassName: "justify-start",
-    img: "",
-    spareImg: "",
-  },
-  {
-    id: 3,
-    title: "My Resume",
-    description: "Are you hiring? Check out ",
-    className: "lg:col-span-2 md:col-span-3 md:row-span-2 flex",
-    imgClassName: "",
-    titleClassName: "justify-end",
-    img: "",
-    spareImg: "",
-  },
-  {
-    id: 4,
-    title: "Tech enthusiast with a passion for development.",
-    description: "",
-    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
-    imgClassName: "",
-    titleClassName: "justify-start",
-    img: "/grid.svg",
-    spareImg: "/b4.svg",
-  },
-
-  {
-    id: 5,
-    title: "Clean Code and Modern Design",
-    description: "Passionate about transforming ideas into reality.",
-    className: "md:col-span-3 md:row-span-2",
-    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
-    titleClassName: "justify-center md:justify-start lg:justify-center",
-    img: "/b5.svg",
-    spareImg: "/grid.svg",
-  },
-  {
-    id: 6,
-    title: "Do you want to start a project together?",
-    description: "",
-    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
-    imgClassName: "",
-    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
-    img: "",
-    spareImg: "",
-  },
-];
-
-export const workExperience = [
-  {
-    id: 1,
-    title: "Pretember Challenge",
-    desc: "Developed CopilotMate, personal AI assistant which integrates tools like a to-do list, ai-enabled spreadsheet, expense tracker and other features.",
-    className: "md:col-span-2",
-    thumbnail: "/exp1.svg",
-  },
-  {
-    id: 2,
-    title: "Open Source",
-    desc: "Developed a JSON Field Renaming Component in Golang for Instill AI, implementing logic for renaming fields with conflict resolution. Additionally, created comprehensive unit tests to ensure its functionality and robustness",
-    className: "md:col-span-2",
-    thumbnail: "/exp2.svg",
-  },
-  {
-    id: 3,
-    title: "Freelance Project",
-    desc: "Developed a web application for a client, handling both design and implementation using React.",
-    className: "md:col-span-2",
-    thumbnail: "/exp3.svg",
-  },
-  {
-    id: 4,
-    title: "Team Led",
-    desc: "Led a team of students in creating a project for a hackathon, focusing on frontend development and project management.",
-    className: "md:col-span-2",
-    thumbnail: "/exp4.svg",
-  },
+ 
 ];
 
 export const socialMedia = [
@@ -124,3 +27,18 @@ export const socialMedia = [
     href: "https://www.linkedin.com/in/lapayne1982",
   },
 ];
+
+export const projects = [
+  {
+    id: 1,
+    img: "",
+    href: "",
+    description: "",
+  },
+  {
+    id: 2,
+    img: "",
+    href: "",
+    description: "",
+  },
+]
