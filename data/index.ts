057254bd93bc187b04b72652cc3f9fe39ab6821a
lee Payne <lapayne@gmail.com>
2024-12-30 21:11:29 +0000

    update images

--- data/index.ts
+++ data/index.ts
@@ -30,7 +30,7 @@ export const socialMedia = [
 export const projects = [
   {
     id: 1,
-    img: "https://images.unsplash.com/photo-1544077960-604201fe74bc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1651&q=80",
+    img: "/projects.jpg",
     href: "https://www.maxpayne.vip",
     description: "A homepage showing off all of the projects he has completed",
     name: "Projects",
