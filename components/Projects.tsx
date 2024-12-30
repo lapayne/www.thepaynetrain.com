a4ed6a31c744956093476a7b72e66df47a455768
Lee Payne <56700654+lapayne@users.noreply.github.com>
2024-12-30 15:12:47 +0000

    Create Projects.tsx

---
+++ components/Projects.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+
+const Projects = () => {
+
+  return (
+    <div className="p-10 bg-white dark:bg-neutral-950 rounded-md relative border border-neutral-800  dark:border-neutral-800 my-10 py-10 flex flex-col items-center">
+    
+      
+    </div>
+  );
+};
+
+export default Projects;
