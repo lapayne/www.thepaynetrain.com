d8d8ca28f5e01d5ad8d9898b47d38ea0974ac021
Lee Payne <56700654+lapayne@users.noreply.github.com>
2025-05-19 20:44:44 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -8,8 +8,17 @@ import Education from "@/components/Education";
 import Work from "@/components/work";
 import Projects from "@/components/Projects";
 import Certs from "@/components/Certs";
-
+import {analytics, logEvent} from "@/firebase";
 export default function Home() {
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const analytics = getAnalytics();
+
+      const handleRouteChange = (url) => {
+        logEvent(analytics, 'page_view', { page_path: url });
+      };
+    }
   return (
     <main className="bg-black-100 relative flex justify-center items-center flex-col overflow-clip mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
